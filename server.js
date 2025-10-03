4c5d69c0ed48a1982e2c3ba15c45706d0eb55a59
xingqiyideemeng <163309175+xingqiyideemeng@users.noreply.github.com>
2025-10-04 00:46:10 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const apiRoutes = require('./routes/api');
+
+const app = express();
+const PORT = 3000;
+
+// 中间件
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client')));
+
+// API 路由
+app.use('/api', apiRoutes);
+
+// 静态文件服务
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
